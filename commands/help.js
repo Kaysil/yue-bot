ec5a0e9e27f984e5339725159677ce9408c1780a
Kaysil <kaysilminer@gmail.com>
2020-09-02 10:29:03 +0700

    Update code

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "Help",
-    description: "help command",
-    command: "help",
-    aliases: ["commands"],
+    description: "Lệnh dùng để nhận danh sách các lệnh có sẵn",
+    commands: ["help", "commands"],
+    usage: "[lệnh]",
     execute: (async (client, message, args) => {
-        
+        message.reply("c")
     })
 };
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 require("dotenv").config();
+const { BOT_PREFIX, BOT_TOKEN } = process.env;
 
 const { Client, Collection }= require("discord.js");
 const fs = require("fs");
 const path = require("path");
 
-const { BOT_PREFIX, BOT_TOKEN } = process.env;
 const { version } = require("./package.json");
 
 const Server = require("./server")();
@@ -20,26 +20,29 @@ logging.log(`Đây là một dự án mã nguồn mở, bạn có thể xem mã 
 logging.log(`(C) 2020 - Made by Kaysil`);
 console.log(`\n`);
 
-// Load tất cả những lệnh trong thư mục commands
-fs.readdirSync(path.join(__dirname, "commands"))
-.filter(command => command.endsWith(".js"))
-.forEach(command => {
-    var commandData = require(`./commands/${command}`);
-    client.command.set(commandData.command, commandData.execute);
+fs.readdirSync(path.join(__dirname, "commands")).filter(file => file.endsWith(".js")).forEach(commandFile => {
+    var commandData = require(path.join(__dirname, "commands", commandFile));
 
-    if (commandData.aliases) {
-        commandData.aliases.forEach(alias => {
-            client.command.set(alias, commandData.execute);
-        });
-    }
+    commandData.commands.forEach(command => {
+        client.commands.set(command, commandData);
+        logging.log(`Đã load xong lệnh "${command}" của file "${commandFile}"`);
+    });
+
+    logging.log(`Đã load xong file "${commandFile}"`);
 });
 
 client.on("ready", () => {
-    logging.log(`Đã dăng nhập với tên người dùng là ${client.user.tag}`);
+    logging.log(`Đã dăng nhập với username là ${client.user.tag}`);
 });
  
 client.on("message", (message) => {
-    logging.log(`Đã nhận được một tin nhắn`);
+    if (!message.content.startsWith(BOT_PREFIX) || message.author.bot) return;
+
+	const args = message.content.slice(BOT_PREFIX.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+    
+    if (!client.commands.has(command)) return;
+    client.commands.get(command).execute(client, message, args);
 });
 
 client.login(BOT_TOKEN);
